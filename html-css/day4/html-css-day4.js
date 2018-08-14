4ed6c4f430da1ed05e94752af568fe9c88daa492
emmadvlpr <emma.phommatha@gmail.com>
2018-08-13 23:22:07 -0600

    HTML-CSS

---
+++ html-css/day4/html-css-day4.js
@@ -0,0 +1,178 @@
+
+/* 03.6.2018. HTML && CSS 1.4
+
+••••• HTML && CSS ••••••
+
+••• CSS is a language that describes the style of an HTML document.
+••• CSS describes how HTML elements should be displayed.
+••• All HTML elements can be considered as boxes. 
+••• In CSS, the term "box model" is used when talking about design and layout.
+••• The CSS box model is essentially a box that wraps around every HTML element: 
+It consists of: margins, borders, padding, and the actual content.
+••• Semantics is the study of the meanings of words and phrases in a language.
+Semantic elements = elements with a meaning.
+
+
+••• The CSS box Model •••
+• Content - The content of the box, where text and images appear.
+• Padding - Clears an area around the content, The padding is transparent.
+• Border - A border that goes around the padding and content.
+• Margin - Clears an area outside the border, the margin is transparent.
+
+
+••• Outline-Style •••
+•• The outline-style property specifies the style of the outline, and can have one of the following values:
+• dotted - Defines a dotted outline
+• dashed - Defines a dashed outline
+• solid - Defines a solid outline
+• double - Defines a double outline
+• groove - Defines a 3D grooved outline
+• ridge - Defines a 3D ridged outline
+• inset - Defines a 3D inset outline
+• outset - Defines a 3D outset outline
+• none - Defines no outline
+• hidden - Defines a hidden outline
+
+
+••• Text Color •••
+•• The color property is used to set the color of the text. The color is specified by:
+• a color name - like "red"
+
+
+••• Text Alignment •••
+•• The text-align property is used to set the horizontal alignment of a text.
+•• A text can be left or right aligned, centered, or justified.
+• When the text-align property is set to "justify", each line is stretched so that every line has equal width, 
+and the left and right margins are straight
+
+
+••• Text Decoration •••
+• The text-decoration property is used to set or remove decorations from text.
+• The value text-decoration: none; is often used to remove underlines from links.
+
+
+••• Text Transformation •••
+• The text-transform property is used to specify uppercase and lowercase letters in a text.
+• It can be used to turn everything into uppercase or lowercase letters, or capitalize the first letter of each word.
+
+
+••• Text Indentation •••
+• The text-indent property is used to specify the indentation of the first line of a text.
+
+
+••• Letter Spacing •••
+• The letter-spacing property is used to specify the space between the characters in a text.
+
+
+••• Line Height •••
+• The line-height property is used to specify the space between lines.
+
+
+••• Text Direction •••
+• The direction property is used to change the text direction of an element.
+
+
+••• Word Spacing •••
+• The word-spacing property is used to specify the space between the words in a text.
+
+
+••• Text Shadow •••
+• The text-shadow property adds shadow to text.
+
+
+••• CSS Font Families •••
+• generic family - a group of font families with a similar look (like "Serif" or "Monospace")
+• font family - a specific font family (like "Times New Roman" or "Arial")
+
+
+••• Font Style •••
+• normal - The text is shown normally
+• italic - The text is shown in italics
+• oblique - The text is "leaning" (oblique is very similar to italic, but less supported)
+
+
+••• Responsive Font Size •••
+• The text size can be set with a vw unit, which means the "viewport width".
+• That way the text size will follow the size of the browser window.
+
+
+••• How To Add Icons •••
+• The simplest way to add an icon to your HTML page, is with an icon library, such as Font Awesome.
+• Add the name of the specified icon class to any inline HTML element (like <i> or <span>).
+
+••• The display Property •••
+• The display property is the most important CSS property for controlling layout.
+
+• The display property specifies if/how an element is displayed.
+
+• Every HTML element has a default display value depending on what type of element it is.
+
+• The default display value for most elements is block or inline.
+
+
+••• The position Property •••
+• The position property specifies the type of positioning method used for an element 
+(static, relative, fixed, absolute or sticky).
+
+
+••• CSS Overflow •••
+•• The overflow property specifies whether to clip content or to add scrollbars when the content 
+of an element is too big to fit in a specified area.
+• visible - Default. The overflow is not clipped. It renders outside the element's box
+• hidden - The overflow is clipped, and the rest of the content will be invisible
+• scroll - The overflow is clipped, but a scrollbar is added to see the rest of the content
+• auto - If overflow is clipped, a scrollbar should be added to see the rest of the content
+
+
+••• The float Property •••
+• The CSS float property specifies how an element should float.
+• The float property is used for positioning and layout on web pages.
+
+
+••• Setting height and width •••
+• The height and width properties are used to set the height and width of an element.
+• The height and width can be set to auto (this is default. Means that the browser calculates the height and width), 
+or be specified in length values, like px, cm, etc., or in percent (%) of the containing block.
+
+
+••• Website Layout •••
+• A website is often divided into headers, menus, content and a footer
+
+
+••• CSS Box Sizing •••
+• The CSS box-sizing property allows us to include the padding and border in an element's total width and height.
+• The box-sizing property allows us to include the padding and border in an element's total width and height.
+• If you set box-sizing: border-box; on an element padding and border are included in the width and height.
+
+
+••• CSS Flexbox Layout Module •••
+•• The Flexible Box Layout Module, makes it easier to design flexible responsive layout structure without using float or positioning.
+•• Before the Flexbox Layout module, there were four layout modes:
+• Block, for sections in a webpage
+• Inline, for text
+• Table, for two-dimensional table data
+• Positioned, for explicit position of an element
+
+•• flex-direction: property defines in which direction the container wants to stack the flex items.
+
+•• flex-wrap: property specifies whether the flex items should wrap or not.
+
+•• flex-flow: property is a shorthand property for setting both the flex-direction and flex-wrap properties.
+
+•• justify-content: property is used to align the flex items.
+
+•• align-items: property is used to align the flex items vertically.
+
+•• align-content: property is used to align the flex lines.
+
+•• Set both: the justify-content and align-items properties to center, and the flex item will be perfectly centered.
+
+•• order: property specifies the order of the flex items.
+
+•• flex-grow: property specifies how much a flex item will grow relative to the rest of the flex items.
+
+•• flex-shrink: property specifies how much a flex item will shrink relative to the rest of the flex items.
+
+•• flex-basis: property specifies the initial length of a flex item.
+*/
+
